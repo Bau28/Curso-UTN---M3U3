6146d5aace41baa62e86fbc7cf8bb15f5ad3a301
Bautista Gomez <bau241006@gmail.com>
2025-03-15 18:56:38 -0300

    crud archivos

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var usuariosModel = require('../../models/usuariosModel');
+var novedadesModel = require('../../models/novedadesModel');
 
-/* GET home page. */
+/* Listar novedades. */
 router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades',{
         layout: 'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        novedades
     });
 });
+
+/*Borrar novedades.*/
+router.get('/eliminar/:id', async (req,res,next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadByID(id);
+    res.redirect('/admin/novedades')
+});
+
 
 module.exports = router;
